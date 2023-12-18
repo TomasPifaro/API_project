8c2e96ff50ecf485813d1c4dd6f0d3df926a8812
TomasPifaro <tomaspifaro@gmail.com>
2023-12-18 15:35:44 +0200

    meow

--- API_project/obj/Debug/net8.0/API_project.AssemblyInfo.cs
+++ API_project/obj/Debug/net8.0/API_project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686e0e3a08059b1e81df3b6a261d248b2186d732")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
